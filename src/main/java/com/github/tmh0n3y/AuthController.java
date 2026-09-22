5c39b955a4b6d34716cfd24d938077a0d276cd0b
Hanna TM <hanna.toth.m@gmail.com>
2026-09-22 17:32:27 +0300

    feat: get jwt header and find x5u value

--- src/main/java/com/github/tmh0n3y/AuthController.java
+++ src/main/java/com/github/tmh0n3y/AuthController.java
@@ -1,14 +1,48 @@
 package com.github.tmh0n3y;
 
+import java.util.Base64;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
 public class AuthController {
 
     @GetMapping("/auth")
-    public String auth() {
-        return "Hello world!";
+    public ResponseEntity<AuthResponse> validateJwt(@RequestHeader(value = "Authorization", required = false) String authHeader) { //get auth header without throwing exception if null
+
+        // is the header format ok
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.badRequest().body(
+                new AuthResponse(false, "Missing or invalid Authorization header format.")
+            );
+        }
+
+        // cut bearer prefix and get token and its parts
+        String token = authHeader.substring(7).trim();
+        String[] parts = token.split("\\."); //[0] = header [1] = payload [2] = signature
+
+        //header contains the x5u value that we need to decode from base64 to standard json
+        String headerJson = new String(Base64.getUrlDecoder().decode(parts[0]));
+
+        //inspect it to get the x5u value
+        String x5uUrl = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode headerNode = mapper.readTree(headerJson);
+            if (headerNode.has("x5u")) {
+                x5uUrl = headerNode.get("x5u").asText();
+            }
+        } catch (Exception e) {
+            //handle exception
+        }
+
+        //after all checks passed its valid!!
+        return ResponseEntity.ok(new AuthResponse(true));
     }
-    
 }
