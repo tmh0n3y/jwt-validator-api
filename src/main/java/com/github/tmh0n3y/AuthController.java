06a6c9a7147b4e5fc8e0b74f85edba34937e3667
Hanna TM <hanna.toth.m@gmail.com>
2026-09-22 17:39:35 +0300

    feat: add more error handling

--- src/main/java/com/github/tmh0n3y/AuthController.java
+++ src/main/java/com/github/tmh0n3y/AuthController.java
@@ -18,28 +18,30 @@ public class AuthController {
 
         // is the header format ok
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return ResponseEntity.badRequest().body(
-                new AuthResponse(false, "Missing or invalid Authorization header format.")
-            );
+            return ResponseEntity.badRequest().body(new AuthResponse(false, "Missing or invalid Authorization header format."));
         }
 
         // cut bearer prefix and get token and its parts
         String token = authHeader.substring(7).trim();
         String[] parts = token.split("\\."); //[0] = header [1] = payload [2] = signature
 
-        //header contains the x5u value that we need to decode from base64 to standard json
-        String headerJson = new String(Base64.getUrlDecoder().decode(parts[0]));
+        //check if the token has 3 parts
+        if (parts.length != 3) {
+            return ResponseEntity.badRequest().body(new AuthResponse(false, "Invalid JWT format."));
+        }
 
-        //inspect it to get the x5u value
+        //header contains the x5u value that we need to decode from base64 to standard json
         String x5uUrl = null;
         try {
+            String headerJson = new String(Base64.getUrlDecoder().decode(parts[0])); 
+
             ObjectMapper mapper = new ObjectMapper();
             JsonNode headerNode = mapper.readTree(headerJson);
             if (headerNode.has("x5u")) {
                 x5uUrl = headerNode.get("x5u").asText();
             }
         } catch (Exception e) {
-            //handle exception
+           return ResponseEntity.badRequest().body(new AuthResponse(false, "Failed to decode or parse JWT header."));
         }
 
         //after all checks passed its valid!!
